6afbb285cb27979e5ad59ca27600013663cf7fd2
Nico Agten <68544113+nagten@users.noreply.github.com>
2023-03-13 14:05:02 +0100

    Fixed formatting somehow formatting got messed up

--- Got-IgnoreUser.user.js
+++ Got-IgnoreUser.user.js
@@ -6,13 +6,13 @@
 // ==/UserScript==
 // 
 
-//Add the user(s) you want to ignore to this array ['user1', 'user2']
+// Add the user(s) you want to ignore to this array ['user1', 'user2']
 var usersToIgnore = ['user1'];
 
-//What do we want to do with their posts: 'hide' or 'replace'
+// What do we want to do with their posts: 'hide' or 'replace'
 var action = 'replace';
 
-//Find all messages via username class
+// Find all messages via "username" class
 var classUsername = document.evaluate("//p[@class='username']",document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
 
 for (var i = 0; i < classUsername.snapshotLength; i++) 
@@ -23,15 +23,15 @@ for (var i = 0; i < classUsername.snapshotLength; i++)
 
 	if(indexOfUsername != -1)
 	{
-		//console.log(i, userName, indexOfUsername, linkToUsername);
+		// console.log(i, userName, indexOfUsername, linkToUsername);
 		if(action == 'hide')
 		{
-			//console.log(linkToUsername.parentNode.parentNode.parentNode);
+			// console.log(linkToUsername.parentNode.parentNode.parentNode);
 			linkToUsername.parentNode.parentNode.parentNode.style['display']= 'none';
 		}
-	else if(action == 'replace')
+		else if(action == 'replace')
 		{
-			//console.log(linkToUsername.parentNode.parentNode.parentNode);
+			// console.log(linkToUsername.parentNode.parentNode.parentNode);
 			linkToUsername.parentNode.parentNode.parentNode.setAttribute('oldInnerHTML', linkToUsername.parentNode.parentNode.parentNode.innerHTML);
 			linkToUsername.parentNode.parentNode.parentNode.innerHTML='<tr><td colspan="2"><i>Ignored user ' + userName + '.</i><br /><br /><a onclick="this.parentNode.innerHTML = this.parentNode.getAttribute(\'oldInnerHTML\');"><b>Click to show ' + userName + '&rsquo;s post</b></a>.</td></tr>';
 		}
